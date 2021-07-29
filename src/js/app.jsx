9cdae7969729eb40d548f75b6a30fd4954892d38
Josiah Swab <josiahjswab@gmail.com>
2021-07-29 14:22:31 -0700

    init

---
+++ src/js/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/styles.scss'
+
+export default class App extends React.Component {
+
+  
+  // your Javascript goes here
+  render() {
+    return (
+      <div className='container'>
+        {/* your JSX goes here */}
+        Welcome to a working app base!
+      </div>
+    );
+  }
+}
