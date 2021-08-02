c86fad27e8169073d28f12cc1d4a607257523158
kgriffcodes <kgriffith1214@gmail.com>
2021-08-02 09:50:02 -0500

    add light bootstrap styling

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import '../css/styles.scss';
 
@@ -5,65 +7,91 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      balance: 0,
-      rate: 0,
-      term: 0,
-      output: 0
+      balance: undefined,
+      rate: undefined,
+      term: undefined,
+      output: undefined
     };
     this.handleChange = this.handleChange.bind(this);
     this.calculate = this.calculate.bind(this);
   }
 
-  calculate(s) {
-    // const balance = this.state.balance;
-    // const rate = this.state.rate;
-    // const term = this.state.term;
-    // const output = this.state.output;
-    
-    // this.setState({
-    //   output: (balance * ((rate(Math.pow((1 + rate), term)))
-    //    / ((Math.pow((1 + rate), term)) - 1)))
-    // });
-    // console.log(this.state.balance);
-  }
-
-  handleChange = (e) => {
-    console.log(e.target);
-    console.log(e.target.name);
-    const {name, value} = e.target;
+  handleChange(e) {
+    // console.log(e.target);
+    // console.log(e.target.name);
+    const { name, value } = e.target;
 
     this.setState({
       [name]: value,
     });
   }
 
+  calculate(s) {
+    // eslint-disable-next-line prefer-destructuring
+    const balance = this.state.balance;
+    const rateInPercent = this.state.rate;
+    const monthRate = ((rateInPercent / 100) / 12);
+    const term = this.state.term;
+    const monthsToPay = (term * 12);
+    let output = this.state.output;
+
+    const numerator = (monthRate * Math.pow((1 + monthRate), monthsToPay));
+    const denominator = ((Math.pow((1 + monthRate), monthsToPay)) - 1);
+    let newOutput = (balance * (numerator / denominator)).toFixed(2);
+    newOutput = `Your monthly payment will be $${newOutput}.`
+  
+
+    this.setState({
+      output: newOutput
+    });
+
+    console.log(newOutput);
+  }
+
   render() {
     return (
       <div className='container'>
-        <h1>Mortgage Calculator</h1>
-        <input
-          type='number'
-          name='balance'
-          value={ this.state.balance }
-          onChange={ this.handleChange }
-          id='balance'
-          className='row'
-        />
-        <input
-          type='number'
-          name='rate'
-          value={ this.state.rate }
-          onChange={ this.handleChange }
-          step='0.01'
-          id='rate'
-          className='row'
-        />
-        <select name='term' className='row'>
-          <option value='15'>15</option>
-          <option value='30'>30</option>
-        </select>
-        <button name='submit' className='row' onClick={ this.calculate() }>Calculate</button>
-        <div name='output' value={ this.state.output } id='output' className='row' />
+        <div className='row col-md-12 text-center pt-5'>
+          <h1>Mortgage Calculator</h1>
+        </div>
+
+        <div className='row pt-5'>
+          <div className='col-md-4'>
+            <input
+              type='number'
+              name='balance'
+              placeholder='Balance'
+              value={ this.state.balance }
+              onChange={ this.handleChange }
+              id='balance'
+            />
+          </div>
+          <div className='col-md-4'>
+            <input
+              type='number'
+              name='rate'
+              placeholder='Rate (in percentage)'
+              value={ this.state.rate }
+              onChange={ this.handleChange }
+              step='0.01'
+              id='rate'
+              className='row'
+            />
+          </div>
+          <div id='selectEl' className='col-md-4'>
+            <label htmlFor='term' className='row'>Term Length (in years)</label>
+            <select name='term' className='row' onChange={ this.handleChange }>
+              <option value='15'>15</option>
+              <option value='30'>30</option>
+            </select>
+          </div>
+        </div> 
+        <div className='row col-md-12 text-center'>
+          <button name='submit' className='btn btn-primary' type='button' onClick={ () => this.calculate() }>Calculate</button>
+        </div>
+        <div className='row col-md-12 text-center pt-5'>
+          <div name='output' value={ this.state.output } id='output' className='row'>{this.state.output}</div>
+        </div>
       </div>
     );
   }
